109adb92a49616c083ff16046d07f0eebc084742
ADIL <kalikulov71@gmail.com>
2022-01-12 01:58:45 +0300

    duru

---
+++ script.js
@@ -0,0 +1,31 @@
+"use strict";
+
+console.log(7+ +"6");
+
+
+
+let incr = 10,
+    decr=10;
+//++incr;
+//--decr;
+console.log(++incr);
+console.log(--decr);
+
+
+
+const a = false,
+
+      
+      
+      c=true;
+
+
+      console.log(2+2*2 !== '7');
+
+
+
+      
+
+
+
+
